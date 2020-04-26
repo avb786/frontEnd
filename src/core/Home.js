197165b8cb823cacf6e4b9b78ab8159852553af2
uday.khandeka <uday.khandeka@scikey.ai>
2020-04-26 21:44:37 +0530

    chore: intial commit by Aayush bhankale

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import "../styles.css"
-import {API} from "../backend"
-export default function Home(){
-    console.log("API is", API)
-return(
-  <div>
-    <h1 className="text-white">Hello  Frontend</h1>
-  </div>
-)
+import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
 
+export default function Home() {
+  console.log("API IS", API);
+
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
 }
-
